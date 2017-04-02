3e545bfbbc512b2366cdd75582d01c2b48d2a02d
Chulift <peeradon.rwpt@gmail.com>
2017-04-01 20:10:25 -0700

    create numberPicker config

---
+++ app/src/main/java/com/example/chulift/demoapplication/Config/NumberPickerConfig.java
@@ -0,0 +1,13 @@
+package com.example.chulift.demoapplication.Config;
+
+
+public class NumberPickerConfig {
+    public static final int MAX_COLUMN = 8;
+    public static final int MAX_SECTION = 50;
+    public static final int MAX_CHOICE = 5;
+    public static final int MIN_COLUMN = 1;
+    public static final int MIN_SECTION = 1;
+    public static final int MIN_CHOICE = 1;
+    public static final int MAX_STUDENT_CODE = 10;
+    public static final int MIN_STUDENT_CODE = 1;
+}
